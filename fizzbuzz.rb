0ae835529cc24cd6c8409fae2b573ce594c68956
James Lenahan <james.k.lenahan@gmail.com>
2019-11-17 17:25:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(int)
   if int % 5 == 0 and int % 3 == 0 
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif int % 3 == 0 
-    puts "Fizz"
+    "Fizz"
   elsif int % 5 == 0 
-    puts "Buzz"
+    "Buzz"
   end
 end
